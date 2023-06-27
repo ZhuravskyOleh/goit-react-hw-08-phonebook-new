a04b3fa364fb8db02493da75ccb61a93a4b479a8
ZhuravskyOleh <zhuravskyoleh@gmail.com>
2023-06-27 12:47:10 +0300

    edits

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/goit-react-hw-08-phonebook">
+        <BrowserRouter basename="/goit-react-hw-08-phonebook-new">
           <App />
         </BrowserRouter>
       </PersistGate>
